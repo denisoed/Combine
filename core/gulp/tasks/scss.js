9e16407e86c00c7cf1067273ae9f0255bc779ac2
Denisoed <denisod93@gmail.com>
2018-11-17 15:59:12 +0600

    Added scss task for gulp

---
+++ core/gulp/tasks/scss.js
@@ -0,0 +1,42 @@
+const config = require('../../../options/config');
+const gulp           = require('gulp'),
+	sass           = require('gulp-sass'),
+    cleanCSS       = require('gulp-clean-css'),
+	rename         = require('gulp-rename'),
+    browserSync    = require('browser-sync'),
+	notify         = require('gulp-notify'),
+	autoprefixer   = require('gulp-autoprefixer');
+		
+let pathDev = config.paths.dev,
+	pathStage = config.paths.staging;
+
+gulp.task('scss', ['styles'], function() {
+	return gulp.src('../../' + pathDev + '/scss/*.scss')
+		.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleanCSS()) // Comment out when debugging
+		.pipe(gulp.dest('../../' + pathStage + '/css'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+
+gulp.task('styles', function() {
+	return gulp.src('../../' + pathDev + '/scss/styles/*.scss')
+		.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('../../' + pathStage + '/css/styles'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('critical', function() {
+	return gulp.src('../../' + pathDev + '/scss/styles/critical/*.scss')
+		.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('../../' + pathStage + '/css/critical'))
+		.pipe(browserSync.reload({stream: true}))
+});
